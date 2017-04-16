bba329899f1a9abdb5a8039bf35a3d782ffe5e9a
Sophia <ssherman@redhat.com>
2017-04-16 12:37:16 -0400

    Add tab switching functionality in chrome app

--- popup.js
+++ popup.js
@@ -1,17 +1,31 @@
 // Used by "popup.html" to alter the states 
 // of the elements of that page
 
+function switchTabs() {
+	$("#tabs li").click(function() {
+		$("#tabs li").removeClass('active');
+		$(this).addClass('active');
+		$(".tabContent").hide();
+		var selectedTab = $(this).find("a").attr("href");
+		$(selectedTab).fadeIn();
+		return false;
+	});
+}
+
 function init() {
+	$(".tabContent").hide();
+	switchTabs();
+
 	var tbody = $("#accounts tbody");
 	if (tbody.children().length == 0) {
-		$('#accounts').find(tbody).append("<tr><td colspan='4'>Add an account!</td></tr>");
+		$('#accounts').find(tbody).append("<tr><td colspan='4'>Uh oh! \
+			Click 'Actions' to add an account!</td></tr>");
 		//$('#accounts').find(tbody).append("<tr><td>1</td><td>United</td></tr>");
 		//$('#content').html('Add an account!');
 	}
 	else {
 		$('#content').html('blah');
 	}
-	
 }
 
 document.addEventListener('DOMContentLoaded', init);
