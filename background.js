9b3eb7679acde3621dec5e05d24a1e560b389599
Sophia <ssherman@redhat.com>
2017-10-30 11:39:45 -0400

    Added OAuth features

--- background.js
+++ background.js
@@ -21,3 +21,20 @@ function sendRequest( url, callback ) {
 	xhr.open("GET", url, true);
 	xhr.send();
 }
+
+function getToken() {
+	chrome.identity.getAuthToken({
+    	interactive: true
+	}, function(token) {
+    if (chrome.runtime.lastError) {
+        alert(chrome.runtime.lastError.message);
+        return;
+    }
+    var x = new XMLHttpRequest();
+    x.open('GET', 'https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=' + token);
+    x.onload = function() {
+        alert(x.response);
+    };
+    x.send();
+});
+}
--- popup.js
+++ popup.js
@@ -69,7 +69,6 @@ function init() {
 		$('#content').html('blah');
 	}
 	createAirlinesDropDown();
-	//submitForm();
 	addAirlineAccount();
 }
 
